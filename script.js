d4760f5e104de5552ba59f748b70bf79fe81be83
Shrey Keny <shreykeny@gmail.com>
2020-02-27 00:45:14 +0530

    miniscule changes

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ let submit = document.getElementById('submit');
 let text = document.getElementById('text');
 let alert = document.getElementById('alert');
 
+let list = [];
 
-
+let i = 0;
 
 submit.onclick = function addItem() {
 
@@ -19,6 +20,7 @@ submit.onclick = function addItem() {
 
     else {
         newItem(text.value);
+        i++;
         text.value = "";
     }
 
@@ -34,19 +36,29 @@ text.addEventListener("keyup", addItem => {
 
 let newItem = text => {
 
+    list.push(text);
+
     let div = document.createElement('div');
     let p = document.createElement('input');
     let span = document.createElement('span');
 
     p.setAttribute('type', 'checkbox');
     p.setAttribute('id', 'checkbox');
-    span.innerHTML = text;
+
+
+
+    span.setAttribute('id', i);
+    span.innerHTML = list[i];
 
     div.appendChild(p);
     div.appendChild(span);
+    div.setAttribute('id', i);
 
     item.appendChild(div);
 
 }
 
+function myFunction(i) {
+    console.log(document.getElementById(i));
 
+}
